496406c666f80783507533c697a4a694a72331b3
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-22 18:25:37 -0800

    Created Edit and Delete Account buttons to user page.  Added methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, Alert: "Account successfully Deleted!"
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
